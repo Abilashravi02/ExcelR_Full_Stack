782b64f06d334b64286ca7d001d34ed90961764b
Abilashravi02 <abilash0293@gmail.com>
2023-08-22 10:58:02 +0530

    Logical operaters and if else condition

---
+++ 1.Languagefundamentals/13.if-and-else/app1/src/G.java
@@ -0,0 +1,16 @@
+class G
+{
+    public static void main(String[] args) 
+    {
+        if(false)
+        {
+        System.out.println("from if");
+        }
+        System.out.println("asd");
+        else
+        {
+        System.out.println("from else");
+        }
+        System.out.println("main end");
+    }
+}
---
+++ 1.Languagefundamentals/13.if-and-else/app1/src/I.java
@@ -0,0 +1,20 @@
+class I
+{
+    public static void main(String[] args) 
+    {
+        if(true)
+        if(true)
+        {
+        System.out.println("if");
+        }
+        else
+        {
+        System.out.println("else1");
+        }
+         else
+        {
+        System.out.println("else2");
+        }
+       
+    }
+}
