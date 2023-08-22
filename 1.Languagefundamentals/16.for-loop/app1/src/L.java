0d782f47be549ba401240ee860406170dc0c1b0a
Abilashravi02 <abilash0293@gmail.com>
2023-08-23 00:05:38 +0530

    if-else-if and if-else if-esle if-else and For loop

---
+++ 1.Languagefundamentals/16.for-loop/app1/src/L.java
@@ -0,0 +1,14 @@
+class L
+{
+    public static void main(String[] args) 
+    {
+        int i,j;
+        for(i = 0,j = 10; j > 2; j--, i++)
+        {
+            System.out.println(i + ":" + j);
+        }
+           
+         System.out.println(i + ":" + j);
+       
+    }
+}
---
+++ 1.Languagefundamentals/16.for-loop/app1/src/R.java
@@ -0,0 +1,14 @@
+class R
+{
+    public static void main(String[] args) 
+    {
+        
+        if((false == false && false == false )&& (false == false && false == false ) )
+        {
+            System.out.println(true);
+        }
+         else{  
+         System.out.println("hello");
+       }
+    }
+}
