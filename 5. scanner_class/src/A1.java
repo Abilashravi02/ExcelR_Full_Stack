44e17b360f8cbc6707b1150abc261e89a66bb449
Abilashravi02 <abilash0293@gmail.com>
2023-09-09 10:10:32 +0530

    Java

---
+++ 5. scanner_class/src/A1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class A1
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        //System.out.println("Hello World!");
+    }
+}
