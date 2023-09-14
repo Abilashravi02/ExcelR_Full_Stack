8db8b6680f963692e87d4b1683c45771f24c4ccf
Abilashravi02 <abilash0293@gmail.com>
2023-09-14 12:18:48 +0530

    core java

---
+++ 7. packages_and_access_levels/app3/src/rst/U.java
@@ -0,0 +1,13 @@
+package rst;
+import excelr.N;
+class U extends excelr.N
+{
+    public static void main(String[] args) 
+    {
+        N n1 = new N();
+        U u1 = new U();
+        System.out.println(n1.i);
+        System.out.println(u1.i);
+        System.out.println("done");
+    }
+}
